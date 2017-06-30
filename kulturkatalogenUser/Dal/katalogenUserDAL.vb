21dbc9fe94b556306eab1987106f66318c4cedd4
Andreas Josefsson <andreas@barnensbibliotek.se>
2017-06-30 16:17:46 +0200

    20170630 backup

--- kulturkatalogenUser/Dal/katalogenUserDAL.vb
+++ kulturkatalogenUser/Dal/katalogenUserDAL.vb
@@ -1,6 +1,6 @@
 ﻿Public Class katalogenUserDAL
-    'Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=dnndev_v902.me;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
-    Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
+    Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=dnndev_v902.me;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
+    'Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
     Private _linqObj As New kk_aj_katalogenUserLinqDataContext(_connectionString)
 
 #Region "användardata"
